c36722f2c00be5a5b8096419c9fa6bc3dde5eb3d
teena1994 <teenathomas.mec@gmail.com>
2023-08-17 15:18:28 +0100

    Updated files for machine utilization api and readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 API using Node.js and Express.js that interacts with a Microsoft SQL Server database and provides aggregated information for a specified metric. The API retrieve data from the SQL database, calculate the designated metric like machine utilization, and return the aggregated result.
 
+------------------------------API Documentation------------------------------
+
+METHOD : GET
+
+DESCRIPTION : Calculate machine utilization by machine name, date filter and pagination
+
+API EXAMPLE : http://localhost:3000/api/aggregate?machine=Moulding_Geiss2&startDate=2022-06-13&endDate=2023-08-17&pagination=true&pageNumber=1&pageSize=5
+
+PARAMS : 
+    machine - Machine name. Eg: Moulding_Geiss2
+
+    startDate: Date in format yyyy-mm-dd. Eg: 2022-06-13
+
+    endDate: Date in format yyyy-mm-dd. Eg: 2023-08-17
+
+    pagination: Boolean value which should be true if pagination is needed. If not passed will be considered as false and is mandatory if pageNumber or  pageSize is passed in the request.
+
+    pageNumber : Number. By default will be considered as 1.
+
+    pageSize: Number. By default will be considered as 10.
+
+----------------------------------------------------------------------------------
+
+Import Maverick.postman_collection.json in postman for testing API's
+
+----------------------------------------------------------------------------------
--- controller/machineController.js
+++ controller/machineController.js
@@ -1,45 +1,98 @@
 const sql = require('mssql');
 const dbConfig = require('../config/dbConfig');
 /*
-1. Get list of usa state details from mongo db collection 'state_details'.
-2. Filter specific state name from the above array to get the coordinate details
+1. Create sql query based on API req Params.
+2. Filter documents from SQL and Calculate machine utilization
 */
 exports.getMachineUtilization = async (req, res) => {
     try {
+        let mandatoryParamsRes = await checkForMandatoryParameters(req);
 
-        console.log('Calculating machine utilization!');
+        if (mandatoryParamsRes.missingMandatoryParams) {
 
-        const machineName = req.query.machine,
-              startDate = req.query.startDate,
-              endDate = req.query.endDate,
-              pageNumber = parseInt(req.query.pageNumber) || 1,
-              pageSize = parseInt(req.query.page_size) || 10,
-              pagination = req.query.pagination;
+            res.status(400).json({ success: false, message: mandatoryParamsRes.errMessage });
 
-        console.log(`Query details are: machineName: ${machineName}, 
+        } else {
+
+            const machineName = req.query.machine,
+                startDate = req.query.startDate,
+                endDate = req.query.endDate,
+                pageNumber = parseInt(req.query.pageNumber) || 1,
+                pageSize = parseInt(req.query.pageSize) || 10,
+                pagination = req.query.pagination;
+
+            console.log(`Query details are: machineName: ${machineName}, 
                                         startDate: ${startDate}, 
                                         endDate: ${endDate},
                                         pagination: ${pagination},
                                         pageNumber: ${pageNumber},
                                         pageSize: ${pageSize}`);
 
-        // Connect to the database
-        await sql.connect(dbConfig);
+            // Connect to the sql database
+            await sql.connect(dbConfig);
 
-        // Calculate the offset for pagination
-        const offset = (pageNumber - 1) * pageSize;
+            // Calculate the offset for pagination
+            const offset = (pageNumber - 1) * pageSize;
+
+            //Create the sql query based on conditions
+            let query = await createSqlQuery(machineName, startDate, endDate, pagination, pageNumber, pageSize, offset);
+
+            //Get response from sql based on query created
+            const result = await sql.query(query, { machineName, startDate, endDate, offset, pageSize });
+
+            if (result.recordset.length === 0) {
+
+                res.status(200).json({ success: false, message: 'No records found for the given filters' });
+
+            } else {
+
+                const operationalDuration = result.recordset[0].operationalDuration,
+                    nonOperationalDuration = result.recordset[0].nonOperationalDuration;
+
+                // Calculate machine utilization
+                const utilization = (operationalDuration / 3600) / ((operationalDuration / 3600) + (nonOperationalDuration / 3600));
+
+                res.status(200).json({ success: true, machine: machineName, result: result.recordsets[0], utilization: utilization.toFixed(2) });
+
+            }
+        }
+    } catch (err) {
+        res.status(400).json({ success: false, message: err.message });
+    }
+};
+
+//Check whether any mandatory parameters are missing while calling API
+checkForMandatoryParameters = async function (req) {
+    try {
+        console.log('checking mandatory parameters.');
+
+        var mandatoryParamsRes = { missingMandatoryParams: false, errMessage: '' };
+
+        if ((req.query.pagination === undefined) && ((req.query.pageNumber !== undefined) || (req.query.pageSize !== undefined))) {
+            mandatoryParamsRes.missingMandatoryParams = true;
+            mandatoryParamsRes.errMessage = `Missing parameter 'pagination'!`
+        }
+        return mandatoryParamsRes;
+    } catch (e) {
+        throw e;
+    }
+}
+
+//Create sql query based on API req params 
+createSqlQuery = async function (machineName, startDate, endDate, pagination, pageNumber, pageSize, offset) {
+    try {
 
         let conditionalClauses = `WHERE 1=1`;
 
         if (machineName) {
             conditionalClauses += ` AND machine_name = '${machineName}'`;
         }
-      
+
         if (startDate && endDate) {
             conditionalClauses += ` AND start_time >= '${startDate}' AND end_time <= '${endDate}'`;
         }
-        
-        if(pagination){
+
+        if (pagination) {
             conditionalClauses += `
             GROUP BY
                 machine_name
@@ -47,38 +100,17 @@ exports.getMachineUtilization = async (req, res) => {
                 operationalDuration DESC
             OFFSET ${offset} ROWS FETCH NEXT ${pageSize} ROWS ONLY`;
         }
-        
+
         // Query for operational and non-operational durations
         const query = `
         SELECT SUM(CASE WHEN status = 'operational' THEN duration ELSE 0 END) AS operationalDuration,
                SUM(CASE WHEN status = 'non_operational' THEN duration ELSE 0 END) AS nonOperationalDuration
         FROM state_change ${conditionalClauses} `;
 
-        //console.log(query);
+       // console.log(query);
 
-        const result = await sql.query(query, {
-            machineName,
-            startDate,
-            endDate,
-            offset,
-            pageSize
-        });
-
-        if (result.recordset.length === 0) {
-            res.status(404).json({ message: 'No records found for the given filters' });
-        } else {
-        const operationalDuration = result.recordset[0].operationalDuration;
-        const nonOperationalDuration = result.recordset[0].nonOperationalDuration;
-    
-        // Calculate machine utilization
-        const utilization = (operationalDuration / 3600) / ((operationalDuration / 3600) + (nonOperationalDuration / 3600));
-    
-        res.status(200).json({ machine: machineName, result: result.recordsets[0], utilization: utilization.toFixed(2) });
-       
+        return query;
+    } catch (e) {
+        throw e;
     }
-
-    } catch (err) {
-        res.status(400).json({ success: false, message: err.message });
-    }
-};
-
+}
